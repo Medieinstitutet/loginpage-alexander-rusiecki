479d13fc68fc7e5125be533a888690227683da9f
Alexander Rusiecki <54362381+alexander-rusiecki@users.noreply.github.com>
2021-11-30 11:23:02 +0100

    change username availability function

--- scripts/controllers/checkUsernameAvailability.js
+++ scripts/controllers/checkUsernameAvailability.js
@@ -1,12 +1,15 @@
 import { fetchLocalStorage } from '../model/fetchLocalStorage.js';
 import { registerNewUser } from '../controllers/registerNewUser.js';
+import { renderErrorMessage } from '../controllers/renderErrorMessage.js';
 
 export const checkUsernameAvailability = (name, password) => {
   let registeredUsers = fetchLocalStorage('registeredUsers');
-  let nameIsAvailable = registeredUsers.some(
-    registeredName => registeredName !== name
+  let nameNotAvailable = registeredUsers.some(registeredUser =>
+    Object.values(registeredUser).includes(name)
   );
-  if (nameIsAvailable) {
+  if (nameNotAvailable) {
+    renderErrorMessage();
+  } else {
     const newUser = {
       name,
       password,
