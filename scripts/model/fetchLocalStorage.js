aae08c0a3b136af2e7a40423e65cc541d7e9620c
Alexander Rusiecki <54362381+alexander-rusiecki@users.noreply.github.com>
2021-11-30 04:37:21 +0100

    move array of registered users to own file

--- scripts/model/fetchLocalStorage.js
+++ scripts/model/fetchLocalStorage.js
@@ -1,20 +1,8 @@
+import { registeredUsers } from './registeredUsers.js';
+
 export const fetchLocalStorage = key => {
   if (localStorage.getItem(key) === null) {
-    localStorage.setItem(
-      'registeredUsers',
-      JSON.stringify([
-        {
-          name: 'janne',
-          password: 'test',
-          isLoggedIn: false,
-        },
-        {
-          name: 'alex',
-          password: 'psw123',
-          isLoggedIn: false,
-        },
-      ])
-    );
+    localStorage.setItem('registeredUsers', JSON.stringify(registeredUsers));
     return JSON.parse(localStorage.getItem(key));
   } else {
     return JSON.parse(localStorage.getItem(key));
---
+++ scripts/model/registeredUsers.js
@@ -0,0 +1,12 @@
+export const registeredUsers = [
+  {
+    name: 'janne',
+    password: 'test',
+    isLoggedIn: false,
+  },
+  {
+    name: 'alex',
+    password: 'psw123',
+    isLoggedIn: false,
+  },
+];
