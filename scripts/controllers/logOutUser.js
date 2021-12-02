551985afff2ce873ac0a2811b42a9b445aa2c7f7
Alexander Rusiecki <54362381+alexander-rusiecki@users.noreply.github.com>
2021-12-02 04:45:15 +0100

    finish functionality, WIP: css

--- scripts/controllers/logOutUser.js
+++ scripts/controllers/logOutUser.js
@@ -1,11 +1,6 @@
 import { fetchLocalStorage } from '../model/fetchLocalStorage.js';
 import { updateLocalStorage } from '../model/updateLocalStorage.js';
-import { renderForm } from '../view/renderForm.js';
-import { renderFooter } from '../view/renderFooter.js';
-import { displayLoggedInUser } from '../view/displayLoggedInUser.js';
-import { checkUserCredentials } from '../controllers/checkUserCredentials.js';
-import { clearInputField } from '../utils/clearInputField.js';
-import { checkUsernameAvailability } from '../controllers/checkUsernameAvailability.js';
+import { checkIfUserLoggedIn } from '../controllers/checkIfUserLoggedIn.js';
 
 export const logOutUser = user => {
   let registeredUsers = fetchLocalStorage('registeredUsers');
@@ -20,44 +15,5 @@ export const logOutUser = user => {
   });
   updateLocalStorage('registeredUsers', updatedArray);
   registeredUsers = fetchLocalStorage('registeredUsers');
-  const rootDiv = document.getElementById('root');
-  rootDiv.innerHTML = '';
-  const loginForm = renderForm(
-    'userName',
-    'enter username',
-    'userPassword',
-    'enter password',
-    'login'
-  );
-  const registerNewUserForm = renderForm(
-    'newUserName',
-    'choose username',
-    'newUserPassword',
-    'choose password',
-    'register'
-  );
-  renderFooter();
-  displayLoggedInUser(registeredUsers);
-
-  loginForm.addEventListener('submit', e => {
-    e.preventDefault();
-    const name = loginForm.userName.value;
-    const password = loginForm.userPassword.value;
-    if (name && password) {
-      checkUserCredentials(name, password);
-      // clearInputField('userName');
-      // clearInputField('userPassword');
-    }
-  });
-
-  registerNewUserForm.addEventListener('submit', e => {
-    e.preventDefault();
-    const name = registerNewUserForm.newUserName.value;
-    const password = registerNewUserForm.newUserPassword.value;
-    if (name && password) {
-      checkUsernameAvailability(name, password);
-      // clearInputField('newUserName');
-      // clearInputField('newUserPassword');
-    }
-  });
+  checkIfUserLoggedIn(registeredUsers);
 };
--- scripts/view/renderStartPage.js
+++ scripts/view/renderStartPage.js
@@ -1,10 +1,12 @@
 import { logOutUser } from '../controllers/logOutUser.js';
+import { renderFooter } from '../view/renderFooter.js';
 
 export const renderStartPage = loggedInUser => {
   const { name, isLoggedIn, imgUrl } = loggedInUser;
   if (isLoggedIn) {
-    const header = document.querySelector('header');
-    const main = document.querySelector('main');
+    const rootDiv = document.getElementById('root');
+    const header = document.createElement('header');
+    const main = document.createElement('main');
 
     const avatar = document.createElement('img');
     avatar.src = imgUrl;
@@ -15,12 +17,15 @@ export const renderStartPage = loggedInUser => {
     const nameHeading = document.createElement('h1');
     nameHeading.textContent = `Welcome ${name}!`;
 
+    rootDiv.innerHTML = '';
+
+    header.append(avatar, logOutButton);
+    main.append(nameHeading);
+    rootDiv.append(header, main);
+    renderFooter();
     logOutButton.addEventListener('click', () => {
+      rootDiv.innerHTML = '';
       logOutUser(loggedInUser);
     });
-    header.innerHTML = '';
-    header.append(avatar, logOutButton);
-    main.innerHTML = '';
-    main.append(nameHeading);
   }
 };
